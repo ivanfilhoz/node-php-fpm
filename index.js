365ad5df7c4b023cd54b45476465e5357b9ae31f
Ivan Filho <contato@ivanfilho.com>
2017-10-25 20:39:59 -0300

    changed to http format

--- index.js
+++ index.js
@@ -7,80 +7,118 @@ const defaultOptions = {
   skipCheckServer: true
 }
 
-module.exports = function (param = {}) {
-  const options = Object.assign({}, defaultOptions, param)
-  const loader = fastCgi(options)
+module.exports = function (userOptions = {}) {
+  const options = Object.assign({}, defaultOptions, userOptions)
   const fpm = new Promise((resolve, reject) => {
+    const loader = fastCgi(options)
     loader.on('ready', () => resolve(loader))
     loader.on('error', reject)
   })
 
-  return {
-    request: async function (param = {}) {
-      let params = {
-        script: path.join(options.documentRoot, 'index.php'),
-        method: 'GET',
-        contentType: 'text/html'
-      }
-      if (typeof param === 'object') {
-        Object.assign(params, param)
-      } else if (typeof param === 'string') {
-        params.script = param
-      }
+  return async function (req, res) {
+    let params = {
+      uri: req.url
+    }
 
-      if (!path.isAbsolute(params.script)) {
-        params.script = path.join(options.documentRoot, params.script)
-      }
-      if (!params.uri) {
-        params.uri = params.script.match(/\/[^/]+$/)[0]
-      }
+    if (!params.uri || !params.uri.startsWith('/')) {
+      throw new Error('invalid uri')
+    }
 
-      const headers = Object.assign({
-        REQUEST_METHOD: params.method,
-        CONTENT_TYPE: params.contentType,
-        SCRIPT_FILENAME: params.script,
-        SCRIPT_NAME: params.script.split('/').pop(),
-        REQUEST_URI: params.uri,
-        DOCUMENT_URI: params.uri,
-        DOCUMENT_ROOT: options.documentRoot,
-        HTTP_HOST: params.host,
-        SERVER_PROTOCOL: 'HTTP/1.1',
-        GATEWAY_INTERFACE: 'CGI/1.1',
-        SERVER_SOFTWARE: 'php-fpm for Node',
-        REDIRECT_STATUS: 200
-      }, params.headers || {})
-
-      if (params.debug) {
-        console.log(headers)
-      }
-
-      const php = await fpm
-      return new Promise(function (resolve, reject) {
-        php.request(headers, function (err, request) {
-          if (err) { reject(err) }
-          var output = ''
-          var errors = ''
-
-          request.stdout.on('data', function (data) {
-            output += data.toString('utf8')
-          })
-
-          request.stderr.on('data', function (data) {
-            errors += data.toString('utf8')
-          })
-
-          request.stdout.on('end', function () {
-            output = output.replace(/^[\s\S]*?\r\n\r\n/, '')
-            if (errors) {
-              reject(new Error(errors))
-            } else { resolve(output) }
-          })
-
-          if (params.method in ['POST', 'PUT', 'PATCH']) {
-            request.stdin.write(params.body, 'utf8')
+    if (options.rewrite) {
+      const rules = Array.isArray(options.rewrite)
+        ? options.rewrite : [options.rewrite]
+      for (const rule of rules) {
+        const match = params.uri.match(rule.search || /.*/)
+        if (match) {
+          let result = rule.replace
+          for (const index in match) {
+            const selector = new RegExp(`\\$${index}`, 'g')
+            result = result.replace(selector, match[index])
           }
+          params.outerUri = params.uri
+          params.uri = result
+          break
+        }
+      }
+    }
+
+    if (params.uri.indexOf('?') !== -1) {
+      params.document = params.uri.split('?')[0]
+      params.query = params.uri
+        .slice(params.document.length + 1)
+        .replace(/\?/g, '&')
+    }
+
+    if (!params.script) {
+      params.script = path.join(options.documentRoot, params.document || params.uri)
+    }
+
+    const headers = {
+      REQUEST_METHOD: req.method,
+      DOCUMENT_ROOT: options.documentRoot,
+      SCRIPT_FILENAME: params.script,
+      SCRIPT_NAME: params.script.split('/').pop(),
+      REQUEST_URI: params.outerUri || params.uri,
+      DOCUMENT_URI: params.document || params.uri,
+      QUERY_STRING: params.query,
+      HTTP_HOST: req.headers.host,
+      REQUEST_SCHEME: req.protocol,
+      HTTPS: req.protocol === 'https' ? 'on' : undefined,
+      REMOTE_ADDR: req.connection.remoteAddress,
+      REMOTE_PORT: req.connection.remotePort,
+      SERVER_NAME: req.connection.domain,
+      SERVER_PROTOCOL: 'HTTP/1.1',
+      GATEWAY_INTERFACE: 'CGI/1.1',
+      SERVER_SOFTWARE: 'php-fpm for Node',
+      REDIRECT_STATUS: 200
+    }
+
+    for (const header in headers) {
+      if (typeof headers[header] === 'undefined') { delete headers[header] }
+    }
+
+    if (options.debug) {
+      console.log(headers)
+    }
+
+    const php = await fpm
+    return new Promise(function (resolve, reject) {
+      php.request(headers, function (err, request) {
+        if (err) { reject(err) }
+        var output = ''
+        var errors = ''
+
+        if (params.method in ['POST', 'PUT', 'PATCH']) {
+          request.stdin.write(params.body, 'utf8')
+        }
+
+        request.stdout.on('data', function (data) {
+          output += data.toString('utf8')
+        })
+
+        request.stderr.on('data', function (data) {
+          errors += data.toString('utf8')
+        })
+
+        request.stdout.on('end', function () {
+          if (errors) { reject(new Error(errors)) }
+
+          const head = output.match(/^[\s\S]*?\r\n\r\n/)[0]
+          const parseHead = head.split('\r\n').filter(_ => _)
+          for (const item of parseHead) {
+            const pair = item.split(': ')
+            res.setHeader(pair[0], pair[1])
+            if (pair[0] === 'Status') {
+              res.statusCode = parseInt(pair[1].match(/\d+/)[0])
+            }
+          }
+          const body = output.slice(head.length)
+          res.write(body)
+          res.end()
+
+          resolve({ headers, body })
         })
       })
-    }
+    })
   }
 }
